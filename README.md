78105d79d39891852166806485bfb2db4e7e68f8
Michael Vega <michaelvega8888@gmail.com>
2022-05-17 01:26:37 -0400

    add index.js add package.json add package-lock.json update README.md add states.opml

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # PUGS
 Premature Utilities Geared for Servers. The PUGS back-end testing framework. 
+Basically a locally hosted API manager. 
+
+### Start Guide: 
+1. Change path variable to whatever the test file is. For example:
+```javascript
+let path = "../res/states.opml"  // Path to test file (res)
+```
+2. Change the URL variable to include extension for API. For example:
+```javascript
+url = url + "API/opml2json"  // URL extension for API
+```
+
+```Note:``` _Default root link is localhost._ 
---
+++ src/index.js
@@ -0,0 +1,29 @@
+
+const request = require("request");
+const fs = require("fs");
+
+function updateClient(postData){
+    var clientServerOptions = {
+        uri: url,
+        body: postData,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+    }
+    request(clientServerOptions, function (error, response) {
+        console.log(error,response.body);
+        return;
+    });
+}
+
+
+let url = "http://localhost:8080/";
+let path = "../res/states.opml"  // Path to test file (res)
+
+// read XML from a file
+const req_data = fs.readFileSync(path, "utf8");
+req_data.trim();
+
+console.log(req_data);
+updateClient(req_data);
